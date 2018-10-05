d0cd9976578367a26bea371e18f5b9ff12ddeee0
Muhammad Zeeshan <m.zeeshanishaq5@gmail.com>
2018-10-05 18:27:43 +0500

    Tweak home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,7 +87,14 @@
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">Transfers</a>
+
+
+
+                    <a href="https://laravel-news.com">   This is Transfers   </a>
+
+
+
+
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
