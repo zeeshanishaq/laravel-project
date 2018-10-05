389b1a882de6760d9901f8c1a3031e75920d6a65
Muhammad Zeeshan <m.zeeshanishaq5@gmail.com>
2018-10-05 18:25:41 +0500

    Update Main Menu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,7 +87,11 @@
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">Transfers</a>
+
+                    <a href="https://laravel-news.com">
+                    New Transfers
+                    </a>
+
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
